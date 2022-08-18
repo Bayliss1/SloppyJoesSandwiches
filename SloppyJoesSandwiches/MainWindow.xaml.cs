aab02b69908ab2c8aa745741028d7f6dd97de856
Owen <owen.bayliss@sky.com>
2022-08-18 22:59:00 +0100

    Updated.

--- SloppyJoesSandwiches/MainWindow.xaml.cs
+++ SloppyJoesSandwiches/MainWindow.xaml.cs
@@ -15,31 +15,6 @@ using System.Windows.Shapes;
 
 namespace SloppyJoesSandwiches
 {
-    class MenuiItem
-    {
-        public Random Randomizer = new Random();
-
-        public string[] Proteins = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
-        public string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
-        public string[] Breads = { "rye", "white", "wheat", "pumpernickel", "a roll" };
-
-        public string Description = "";
-        public string Price;
-
-        public void Generate()
-        {
-            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
-            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
-            string randomBread = Breads[Randomizer.Next(Breads.Length)];
-            Description = randomProtein = " with " + randomCondiment + " on " + randomBread;
-
-            decimal bucks = Randomizer.Next(2, 5);
-            decimal cents = Randomizer.Next(1, 98);
-            decimal price = bucks + (cents * 0.01M);
-            Price = price.ToString("c");
-        }
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -59,12 +34,40 @@ namespace SloppyJoesSandwiches
             for (int i = 0; i < 5; i++)
             {
                 menuItems[i] = new MenuItem();
-                if (i >=3)
+                if (i >= 3)
                 {
                     menuItems[i].Breads = new string[] { "plain bagel", "onion bagel", "pumpernickel bagel", "everything bagel" };
                 }
                 menuItems[i].Generate();
             }
+
+            item1.Text = menuItems[0].Description;
+            price1.Text = menuItems[0].Price;
+            item2.Text = menuItems[1].Description;
+            price2.Text = menuItems[1].Price;
+            item3.Text = menuItems[2].Description;
+            price3.Text = menuItems[2].Price;
+            item4.Text = menuItems[3].Description;
+            price4.Text = menuItems[3].Price;
+            item5.Text = menuItems[4].Description;
+            price5.Text = menuItems[4].Price;
+
+            MenuItem specialMenuItem = new MenuItem();
+            {
+                string[] Proteins = new string[] { "Organic ham", "Mushroom patty", "Mortadella" };
+                string[] Breads = new string[] { "a gluten free roll", "a wrap", "pita" };
+                string[] Condiments = new string[] { "dijon mustard", "miso dressing", "au jus" };
+            };
+            specialMenuItem.Generate();
+
+            item6.Text = specialMenuItem.Description;
+            price6.Text = specialMenuItem.Price;
+
+            MenuItem guacamoleMenuItem = new MenuItem();
+            guacamoleMenuItem.Generate();
+            guacamolePrice = guacamoleMenuItem.Price;
+
+            guacamole.Text = "Add guacamole for " + guacamoleMenuItem.Price;
         }
     }
 }
